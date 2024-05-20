0f3a62b0de747e6347f33dcd0733fd82bcee6ed4
max <81824845+maxxam123@users.noreply.github.com>
2024-05-20 03:49:52 -0700

    Update main.tf

--- top/terraform-01/main.tf
+++ top/terraform-01/main.tf
@@ -1,7 +1,18 @@
+terraform {
+ required_providers {
+   aws = {
+     source = "hashicorp/aws"
+   }
+ }
+
+ backend "s3" {
+   region = "eu-central-1"
+   key    = "terraform.tfstate"
+ }
+}
+
 provider "aws" {
-  region = "eu-central-1"
-  access_key = ${{ secrets.ACCESS_KEY }}
-  secret_key = ${{ secrets.SECRET_KEY }}
+ region = "eu-central-1"
 }
 
 resource "aws_instance" "ins_1" {
@@ -10,14 +21,5 @@ resource "aws_instance" "ins_1" {
   subnet_id = "subnet-002308d00bad9957e"
   tags = {
     Name = "Ins 1"
-  }
-}
-
-terraform {
-  backend "s3" {
-    bucket = "dunhunag-s3-bucket"
-    key = "dunhunag/terraform/remote/s3/test/terraform.tfstate"
-    region = "eu-central-1"
-    dynamodb_table = "dynamodb-state-locking"
   }
 }
