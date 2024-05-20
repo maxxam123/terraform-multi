2497a421fbaac7ac64f158e7b4cd97ae658209c6
max <81824845+maxxam123@users.noreply.github.com>
2024-05-20 06:24:06 -0700

    Update main.tf

--- top/terraform-01/main.tf
+++ top/terraform-01/main.tf
@@ -1,9 +1,9 @@
 # Comment
-resource "aws_instance" "ins_abc" {
+resource "aws_instance" "ins_abcd" {
   ami = "ami-01e444924a2233b07"
   instance_type = "t2.micro"
   subnet_id = "subnet-002308d00bad9957e"
   tags = {
-    Name = "Ins abc"
+    Name = "Ins abcd"
   }
 }
