6caf8d24f74cc11c8330ba158f5553e12fa015fb
max <81824845+maxxam123@users.noreply.github.com>
2024-05-20 06:26:28 -0700

    Update main.tf

--- top2/main.tf
+++ top2/main.tf
@@ -1,9 +1,9 @@
 
-resource "aws_instance" "ins_a12" {
+resource "aws_instance" "ins_xyz" {
   ami = "ami-01e444924a2233b07"
   instance_type = "t2.micro"
   subnet_id = "subnet-002308d00bad9957e"
   tags = {
-    Name = "Ins a12"
+    Name = "Ins xyz"
   }
 }
