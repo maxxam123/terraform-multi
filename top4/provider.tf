fef7a5a748cf4e480870d103f993f679f7b0115f
max <81824845+maxxam123@users.noreply.github.com>
2024-05-20 08:14:04 -0700

    Create provider.tf

---
+++ top4/provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "dunhuang-terraform-bucket"
+    key            = "dunhuang/terraform/s3/bucket06/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "my-terraform-lock-table6"
+  }
+}
