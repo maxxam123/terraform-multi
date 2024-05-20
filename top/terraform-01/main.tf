4522022e84fcb1cf625b062498a6385387e5590e
max <81824845+maxxam123@users.noreply.github.com>
2024-05-20 04:51:27 -0700

    Update main.tf

--- top/terraform-01/main.tf
+++ top/terraform-01/main.tf
@@ -1,21 +1,5 @@
-terraform {
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-   }
- }
 
- backend "s3" {
-   region = "eu-central-1"
-   key    = "terraform.tfstate"
- }
-}
-
-provider "aws" {
- region = "eu-central-1"
-}
-
-resource "aws_instance" "ins_ONE" {
+resource "aws_instance" "ins_ONE-2" {
   ami = "ami-01e444924a2233b07"
   instance_type = "t2.micro"
   subnet_id = "subnet-002308d00bad9957e"
