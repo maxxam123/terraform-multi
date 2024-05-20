ffdaefe23cb855b47905cd7d5737efcc37386229
max <81824845+maxxam123@users.noreply.github.com>
2024-05-20 06:35:58 -0700

    Update main.tf

--- top2/main.tf
+++ top2/main.tf
@@ -1,9 +1,25 @@
-#  Comment abc
-resource "aws_instance" "ins_xyz" {
+terraform {
+ required_providers {
+   aws = {
+     source = "hashicorp/aws"
+   }
+ }
+
+ backend "s3" {
+   region = "eu-central-1"
+   key    = "terraform.tfstate"
+ }
+}
+
+provider "aws" {
+ region = "eu-central-1"
+}
+
+resource "aws_instance" "ins_nihao" {
   ami = "ami-01e444924a2233b07"
   instance_type = "t2.micro"
   subnet_id = "subnet-002308d00bad9957e"
   tags = {
-    Name = "Ins xyz"
+    Name = "Ins nihao"
   }
 }
