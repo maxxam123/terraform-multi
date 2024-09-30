12ed020ab9ebdb43e744853c221c1e68e6ce0073
max <81824845+maxxam123@users.noreply.github.com>
2024-09-30 11:26:30 -0700

    Update main.tf

--- top2/main.tf
+++ top2/main.tf
@@ -1,9 +1,9 @@
 
-resource "aws_instance" "ins_nihao" {
-  ami = "ami-01e444924a2233b07"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-002308d00bad9957e"
-  tags = {
-    Name = "Ins nihao"
-  }
-}
+#resource "aws_instance" "ins_nihao" {
+#  ami = "ami-01e444924a2233b07"
+#  instance_type = "t2.micro"
+#  subnet_id = "subnet-002308d00bad9957e"
+#  tags = {
+#    Name = "Ins nihao"
+#  }
+#}
