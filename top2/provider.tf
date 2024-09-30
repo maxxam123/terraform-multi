4728d1c045311e204aad0918b1355df3b66d46e9
max <81824845+maxxam123@users.noreply.github.com>
2024-09-30 11:30:38 -0700

    Update provider.tf

--- top2/provider.tf
+++ top2/provider.tf
@@ -6,8 +6,8 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "dunhuang-gansu-terraform-state"
-    key            = "dunhuang/terraform/s3/aws05/terraform.tfstate"
+    key            = "dunhuang/terraform/s3/bucket03/terraform.tfstate"
     region         = "eu-central-1"
-    dynamodb_table = "my-terraform-lock-table3"
+    dynamodb_table = "terraform-state"
   }
 }
